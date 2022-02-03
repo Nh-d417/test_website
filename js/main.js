791711f5318c296bf8aa2e160aaaec31dcfaeb2c
Nh-d417 <alfktngu33@gmail.com>
2022-02-03 23:50:35 +0900

    design_change

--- js/main.js
+++ js/main.js
@@ -1,10 +1,32 @@
+let cursor = document.querySelector(".cursor");
+let logo = document.querySelector(".logo");
+let menu = document.querySelectorAll(".menu ul li");
+
 // 윈도우가 다 로드된 후에 실행 -onload
 window.onload = function(){
-  let cursor = document.querySelector("#cursor");
 
   window.addEventListener("mousemove", mouseFunc, false);
   
   function mouseFunc(e){
     cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    
+  }
+
+}
+
+function onmouseover_event(e){
+  cursor.classList.add("on");
+  // logo.addEventListener("click", e=>{
+
+  // });
+
+  logo.addEventListener("mouseleave", e =>{
+    cursor.classList.remove("on");
+  });
+
+  for (let le of menu) {
+    le.addEventListener("mouseleave", e =>{
+      cursor.classList.remove("on");
+    });
   }
 }
