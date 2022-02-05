320ee0e4caf4ca4b810c7e66178216e6f77e104d
Nh-d417 <alfktngu33@gmail.com>
2022-02-05 23:15:39 +0900

    container1_css

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
 let cursor = document.querySelector(".cursor");
 let logo = document.querySelector(".logo");
 let menu = document.querySelectorAll(".menu ul li");
+let scroll= window.screen.height;
+
 
 // 윈도우가 다 로드된 후에 실행 -onload
 window.onload = function(){
 
   window.addEventListener("mousemove", mouseFunc, false);
-  
+  window.addEventListener("scroll", scrollEvent);
+
   function mouseFunc(e){
     cursor.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+    }
+
+  // 트랜지션으로 스크롤 
+  function scrollEvent(e){
+    console.log(scroll);
+
     
   }
-
 }
 
 function onmouseover_event(e){
